508ba515f9c0b4831b1688ac81799cced566f389
Wangshitao <1714483432@qq.com>
2019-05-05 23:27:12 +0800

    2019.5.5 23:30

--- version2/load_dataset.py
+++ version2/load_dataset.py
@@ -1,4 +1,6 @@
 #load dataset
+from collections import defaultdict
+import os
 def load_dataset(path,name,one_hot=False):
     if name=="mutag":
         dataset = create_mutag(path,one_hot)
@@ -19,8 +21,33 @@ def create_mutag(path,one_hot):
 def get_graph_label(path,name):
     graph_label = list()
     with open(path+name,'r') as f:
-        graph_label = list(filter(lambda x:x.split() if x in f.readlines()))
-
+        for index,line in enumerate(f):
+            graph_label.append((index+1,int(line.split()[0])))
     return graph_label
 
-get_graph_label('./data','MUTAG_graph_labels.txt')
+
+def get_adjancy(path,name):
+    adjancy = defaultdict(list)
+    with open(path+name,'r') as f:
+        for line in f:
+            adjancy[int(line.split(',')[0])].append(int(line.split(',')[1]))
+    return adjancy
+
+def get_node_dict(path,name):
+    node_dict = dict()
+    with open(path+name,'r') as f:
+        for index,line in enumerate(f):
+            node[index+1]=int(line.split()[0])
+    return node_dict
+
+def get_graph_indicator(path,name):
+    indecator = defaultdict(list)
+    with open(path+name,'r') as f:
+        for index,line in enumerate(f):
+            indecator[int(line.split()[0])].append(index+1)
+    return indecator
+
+path = os.getcwd()+'/data/MUTAG_2/'
+
+
+
